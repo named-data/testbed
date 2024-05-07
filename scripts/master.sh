3606c98da856c5a15467c900e24f2df4b9f216f2
Varun Patil <varunpatil@ucla.edu>
2024-05-06 23:58:02 -0700

    master: repo cert

--- scripts/master.sh
+++ scripts/master.sh
@@ -19,6 +19,7 @@ python3 framework/main.py --dry
 # Check and reissue certificates
 bash dist/ndncert/renew.sh
 bash dist/nlsr/renew.sh
+bas dist/ndn-python-repo/renew.sh
 
 # End bootstrapping
 unset TESTBED_BOOTSTRAP
