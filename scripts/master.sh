b64556c569e46d53492fe8e30c79d67774076e5c
Varun Patil <varunpatil@ucla.edu>
2024-05-07 18:47:11 -0700

    master: rm link

--- scripts/master.sh
+++ scripts/master.sh
@@ -38,5 +38,6 @@ date > dist/.master-ready
 sleep 120
 
 mkdir -p "/var/spool/cron/crontabs"
-ln -sf "$(pwd)/scripts/crontab-master" "/var/spool/cron/crontabs/$(whoami)"
+rm -f "/var/spool/cron/crontabs/$(whoami)"
+ln -s "$(pwd)/scripts/crontab-master" "/var/spool/cron/crontabs/$(whoami)"
 exec busybox crond -f -L /dev/stdout
