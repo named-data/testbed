8605e1fe19abab000015b6e82d1f789f80fa2ce5
Varun Patil <varunpatil@ucla.edu>
2024-05-10 13:33:19 -0700

    cron-status: get env

--- scripts/cron-status.sh
+++ scripts/cron-status.sh
@@ -3,6 +3,9 @@
 # Change directory to the root of the project
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
+# Get variables in shell
+source .env
+
 # Save status to file server
 python3 framework/status-json.py > dist/file-server/status.json
 
